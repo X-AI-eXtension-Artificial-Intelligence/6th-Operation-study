ea089d4e153f2e32adedf3f0f8754bd68e367965
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2025-01-07 20:20:46 +0900

    feat: vgg 1주차 코드 업데이트

---
+++ VGG16/1주차/민지/evaluate.py
@@ -0,0 +1,36 @@
+from torch.utils.data import DataLoader
+
+from dataset import data_transform
+from vgg16 import VGG16
+import torch
+
+# setting
+device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+batch_size = 32
+
+# data
+test_set = data_transform(train=False)
+test_loader = DataLoader(test_set, batch_size=batch_size)
+
+# model load
+model = VGG16(base_dim=64).to(device)
+model.load_state_dict(torch.load('./model/vgg16_10.pth', weights_only=True))
+
+# eval
+correct = 0
+total = 0
+
+model.eval()
+
+with torch.no_grad(): # 모델 평가 단계이므로 가중치 업뎃할 필요 없으므로 메모리 사용량과 연산 시간을 줄이기 위해 사용
+    for i, [image, label] in enumerate(test_loader):
+        x = image.to(device)
+        y = label.to(device)
+
+        output = model.forward(x) # output : 모델이 예측한 클래스별 확률
+        _, output_index = torch.max(output, 1) # output 텐서의 가장 큰 값과 해당 인덱스 반환
+
+        total += label.size(0)
+        correct += (output_index == y).sum().float()
+
+    print("### Accuracy of Test Data: {}%".format(100 * correct / total))
